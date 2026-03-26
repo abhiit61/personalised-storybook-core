50b4e260ca6c1c3caac1711ef04d693c523af0a5
Abhishek Upadhyay <abhiit61@gmail.com>
2026-03-26 17:05:43 +0530

    fix cors issue

--- src/main/java/com/fusion/psb/config/StoryBookAppConfig.java
+++ src/main/java/com/fusion/psb/config/StoryBookAppConfig.java
@@ -2,14 +2,15 @@ package com.fusion.psb.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.google.genai.GoogleGenAiChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class StoryBookAppConfig {
+public class StoryBookAppConfig implements WebMvcConfigurer {
 
   @Value("${chat.model}")
   private String chatModel;
@@ -24,4 +25,12 @@ public class StoryBookAppConfig {
     return ChatClient.create(genAiChatModel);
   }
 
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**") // Allow all endpoints
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+        .allowedOriginPatterns("http://localhost:4200")
+        .allowedHeaders("*") // Allow all headers
+        .allowCredentials(true); // Allow cookies
+  }
 }
